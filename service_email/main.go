5158c099ae753662da7dbb2dd2495a818e3510c2
Firewine <>
2021-10-13 14:13:41 +0800

    完成service_email的grpc逻辑和测试

---
+++ service_email/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"microservices_demo_v1/service_email/internal/biz"
+	"microservices_demo_v1/service_email/internal/server"
+	"microservices_demo_v1/service_email/internal/service"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var logger *zap.Logger
+
+func init() {
+	logger = zap.NewExample()
+}
+func main() {
+	var grpcServer *grpc.Server
+
+	emailUseCase := biz.NewEmailUseCase(logger)
+	productService := service.NewEmailService(emailUseCase, logger)
+
+	go func() {
+		addr := "0.0.0.0:" + fmt.Sprint(9001)
+		grpcServer = server.NewGRPCServer(logger, productService)
+		lis, err := net.Listen("tcp", addr)
+		if err != nil {
+			logger.Error("port is used : ", zap.Error(err))
+			return
+		}
+		fmt.Println("started grpc server" + addr)
+		reflection.Register(grpcServer)
+		if err := grpcServer.Serve(lis); err != nil {
+			logger.Error("start grpc failed :%v", zap.Error(err))
+			return
+		}
+
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	fmt.Println("deregister service")
+	grpcServer.GracefulStop()
+}
